74c58860e83e76a67d00002cdfbde3eef74bb12a
Victor Segura <seguravictor57@gmail.com>
2023-03-31 19:47:20 -0600

    Role entity Closes #3

---
+++ src/main/kotlin/com/backend/uniactivosxray/Entities.kt
@@ -0,0 +1,43 @@
+package com.backend.uniactivosxray
+
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "role")
+data class Role(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+    var name: String,
+
+    // Entity Relationship
+    @ManyToMany
+    @JoinTable(
+        name = "role_privilege",
+        joinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "privilege_id", referencedColumnName = "id")]
+    )
+    var privilegeList: Set<Privilege>,
+
+){
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Role) return false
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id?.hashCode() ?: 0
+    }
+
+    override fun toString(): String {
+        return "Role(id=$id, name='$name', privilegeList=$privilegeList)"
+    }
+
+}

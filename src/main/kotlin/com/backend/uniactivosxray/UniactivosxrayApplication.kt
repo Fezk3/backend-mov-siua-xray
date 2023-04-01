5330de23046340afa8824c4d096524e98222063d
Victor Segura <seguravictor57@gmail.com>
2023-03-31 19:26:38 -0600

    inicio

---
+++ src/main/kotlin/com/backend/uniactivosxray/UniactivosxrayApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.uniactivosxray
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UniactivosxrayApplication
+
+fun main(args: Array<String>) {
+	runApplication<UniactivosxrayApplication>(*args)
+}
---
+++ src/test/kotlin/com/backend/uniactivosxray/UniactivosxrayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.uniactivosxray
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UniactivosxrayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

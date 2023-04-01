32ae7c7825e0f0265675f7319be27b4fb9f66ed1
Victor Segura <seguravictor57@gmail.com>
2023-03-31 19:59:43 -0600

    User entity Closes #2

--- src/main/kotlin/com/backend/uniactivosxray/Entities.kt
+++ src/main/kotlin/com/backend/uniactivosxray/Entities.kt
@@ -42,6 +42,7 @@ data class Role(
 
 }
 
+
 @Entity
 @Table(name = "privilege")
 data class Privilege(
@@ -74,6 +75,53 @@ data class Privilege(
 
     override fun toString(): String {
         return "Privilege(id=$id, name='$name', roleList=$roleList)"
+    }
+
+}
+
+
+@Entity
+@Table(name = "users")
+data class User(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: Long? = null,
+    var firstName: String,
+    var lastName: String,
+    var password: String,
+    var email: String,
+    var tokenExpired: Boolean?,
+
+    // Entity Relationship
+    @ManyToMany
+    @JoinTable(
+        name = "user_role",
+        joinColumns = [JoinColumn(name = "user_id", referencedColumnName = "id")],
+        inverseJoinColumns = [JoinColumn(name = "role_id", referencedColumnName = "id")]
+    )
+    var roleList: Set<Role>,
+
+){
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is User) return false
+
+        if (id != other.id) return false
+        if (email != other.email) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + email.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "User(id=$id, firstName='$firstName', lastName='$lastName', password='$password', email='$email', tokenExpired=$tokenExpired, roleList=$roleList)"
     }
 
 }

91d5deb40de5dfe8e1cbfddad77b46337eb81e82
Victor Segura <seguravictor57@gmail.com>
2023-04-18 14:37:53 -0600

    User Repository Closes #13

--- src/main/kotlin/com/backend/uniactivosxray/Repositories.kt
+++ src/main/kotlin/com/backend/uniactivosxray/Repositories.kt
@@ -6,3 +6,8 @@ import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByEmail(@Param("email") email : String) : Optional<User>
+}
+

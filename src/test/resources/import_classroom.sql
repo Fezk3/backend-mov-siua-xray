410d714db3e406a3e70997047e5f1bbeeb698664
DiegoEspinoza10 <diego.espinoza.sanchez@est.una.ac.cr>
2023-04-25 10:41:48 -0600

    Agregando el import de Classroom Closes #24

---
+++ src/test/resources/import_classroom.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO public.classroom (id,classNumber,peopleCapacity,IdCollege) VALUES (1,'A-12',10,1);
+INSERT INTO public.classroom (id,classNumber,peopleCapacity,IdCollege) VALUES (2,'B-11',20,2);
+INSERT INTO public.classroom (id,classNumber,peopleCapacity,IdCollege) VALUES (3,'A-10',30,3);

10b17f8e60590eb902a56edd6a2ae95fda15d014
Victor Segura <seguravictor57@gmail.com>
2023-04-25 17:20:21 -0600

    Test con decorador Transactional

--- src/test/kotlin/com/backend/uniactivosxray/ClassroomRepositoryTest.kt
+++ src/test/kotlin/com/backend/uniactivosxray/ClassroomRepositoryTest.kt
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.jdbc.Sql
+import org.springframework.transaction.annotation.Transactional
 
+@Transactional
 @SpringBootTest
 @Sql("/import_college.sql", "/import_classroom.sql")
 class ClassroomRepositoryTest(

97684c688a6422981c0ac6c68dd22d3a8fb285e7
Victor Segura <seguravictor57@gmail.com>
2023-04-25 12:18:54 -0600

    metodo de findbycollegename en classroom repository

--- src/test/kotlin/com/backend/uniactivosxray/ClassroomRepositoryTest.kt
+++ src/test/kotlin/com/backend/uniactivosxray/ClassroomRepositoryTest.kt
@@ -6,5 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.jdbc.Sql
 
-class ClassroomRepositoryTest {
+@SpringBootTest
+@Sql("/import_classroom.sql", "/import_college.sql", "/import_schedule.sql", "/import_users.sql", "/import_assets.sql", "/import_assetType.sql")
+class ClassroomRepositoryTest(
+    @Autowired
+    val classroomRepository: ClassroomRepository,
+) {
+    @Test
+    fun testClassroomFindAll() {
+        val classroomList: List<Classroom> = classroomRepository.findAll()
+        Assertions.assertTrue(classroomList.size == 2)
+    }
+
+    @Test
+    fun testClassroomFindByCollegeName() {
+        val classroomList: List<Classroom> = classroomRepository.findByCollegeName("College 1")
+        Assertions.assertTrue(classroomList.size == 2)
+    }
 }

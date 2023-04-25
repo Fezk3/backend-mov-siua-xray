b34d1994865192ba3068f9898e4bdec252827aa7
Victor Segura <seguravictor57@gmail.com>
2023-04-25 14:44:36 -0600

    classroom tests Closes #28

--- src/test/kotlin/com/backend/uniactivosxray/ClassroomRepositoryTest.kt
+++ src/test/kotlin/com/backend/uniactivosxray/ClassroomRepositoryTest.kt
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.jdbc.Sql
 
 @SpringBootTest
-@Sql("/import_classroom.sql", "/import_college.sql", "/import_schedule.sql", "/import_users.sql", "/import_assets.sql", "/import_assetType.sql")
+@Sql("/import_college.sql", "/import_classroom.sql")
 class ClassroomRepositoryTest(
     @Autowired
     val classroomRepository: ClassroomRepository,
@@ -15,12 +15,19 @@ class ClassroomRepositoryTest(
     @Test
     fun testClassroomFindAll() {
         val classroomList: List<Classroom> = classroomRepository.findAll()
-        Assertions.assertTrue(classroomList.size == 2)
+        Assertions.assertTrue(classroomList.size == 3)
     }
 
     @Test
     fun testClassroomFindByCollegeName() {
         val classroomList: List<Classroom> = classroomRepository.findByCollegeName("College 1")
-        Assertions.assertTrue(classroomList.size == 2)
+        Assertions.assertTrue(classroomList.size == 1)
     }
+
+    @Test
+    fun testClassroomFindByClassNumber() {
+        val classroom: Classroom = classroomRepository.findByClassNumber("A-12").get()
+        Assertions.assertTrue(classroom.classNumber == "A-12")
+    }
+
 }

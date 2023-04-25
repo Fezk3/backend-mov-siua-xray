3b2a4cf01c78616cafc69751f8babcac7b426b12
Victor Segura <seguravictor57@gmail.com>
2023-04-25 17:20:44 -0600

    sqls con insert normales

--- src/test/resources/import_classroom.sql
+++ src/test/resources/import_classroom.sql
@@ -1,4 +1,4 @@
 
-INSERT INTO public.classroom (id,class_number,people_capacity,id_college) VALUES (1,'A-12',10,1) ON CONFLICT (id) DO NOTHING;
-INSERT INTO public.classroom (id,class_number,people_capacity,id_college) VALUES (2,'B-11',20,2) ON CONFLICT (id) DO NOTHING;
-INSERT INTO public.classroom (id,class_number,people_capacity,id_college) VALUES (3,'A-10',30,3) ON CONFLICT (id) DO NOTHING;
+INSERT INTO public.classroom (id,class_number,people_capacity,id_college) VALUES (1,'A-12',10,1);
+INSERT INTO public.classroom (id,class_number,people_capacity,id_college) VALUES (2,'B-11',20,2);
+INSERT INTO public.classroom (id,class_number,people_capacity,id_college) VALUES (3,'A-10',30,3);

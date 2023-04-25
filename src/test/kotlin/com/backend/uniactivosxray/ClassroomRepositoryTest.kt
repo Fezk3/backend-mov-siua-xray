f8c9a4947e606d6f2f692eb2e278bf85e175d163
Victor Segura <seguravictor57@gmail.com>
2023-04-25 10:54:07 -0600

    estructura base para los test

---
+++ src/test/kotlin/com/backend/uniactivosxray/ClassroomRepositoryTest.kt
@@ -0,0 +1,10 @@
+package com.backend.uniactivosxray
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.jdbc.Sql
+
+class ClassroomRepositoryTest {
+}

d65034d28f4787370b6de17a66f67516605ee93c
Victor Segura <seguravictor57@gmail.com>
2023-04-25 10:34:18 -0600

    resource para college

---
+++ src/test/resources/import_college.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO public.college (id, name) VALUES (1, 'College 1');
+INSERT INTO public.college (id, name) VALUES (2, 'College 2');
+INSERT INTO public.college (id, name) VALUES (3, 'College 3');

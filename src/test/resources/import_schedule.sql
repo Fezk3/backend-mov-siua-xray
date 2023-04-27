4372a83433ec9219e26519aa749f61ce6660f6f6
Victor Segura <seguravictor57@gmail.com>
2023-04-27 09:56:46 -0600

    cambios en las propiedades de los import users y schedule

--- src/test/resources/import_schedule.sql
+++ src/test/resources/import_schedule.sql
@@ -1,7 +1,7 @@
 
 
-INSERT INTO public.schedule (id, courseName, date, startTime, endTime, IdUser, IdClassroom) VALUES (1, 'Math', '2020-05-02', '13:00', '14:40', 1, 1);
-INSERT INTO public.schedule (id, courseName, date, startTime, endTime, IdUser, IdClassroom) VALUES (2, 'Programming Fundamentals', '2020-05-04', '15:00', '16:40', 2, 2);
-INSERT INTO public.schedule (id, courseName, date, startTime, endTime, IdUser, IdClassroom) VALUES (3, 'IT Basics', '2020-05-04', '14:00', '17:40', 3, 3);
-INSERT INTO public.schedule (id, courseName, date, startTime, endTime, IdUser, IdClassroom) VALUES (4, 'Data Base', '2020-05-02', '10:00', '11:40', 4, 2);
-INSERT INTO public.schedule (id, courseName, date, startTime, endTime, IdUser, IdClassroom) VALUES (5, 'Networking', '2020-05-02', '14:00', '15:40', 4, 3);
+INSERT INTO public.schedule (id, course_name, date, start_time, end_time, id_User, id_classroom) VALUES (1, 'Math', '2020-05-02', '13:00', '14:40', 1, 1);
+INSERT INTO public.schedule (id, course_name, date, start_time, end_time, id_User, id_classroom) VALUES (2, 'Programming Fundamentals', '2020-05-04', '15:00', '16:40', 2, 2);
+INSERT INTO public.schedule (id, course_name, date, start_time, end_time, id_User, id_classroom) VALUES (3, 'IT Basics', '2020-05-04', '14:00', '17:40', 3, 3);
+INSERT INTO public.schedule (id, course_name, date, start_time, end_time, id_User, id_classroom) VALUES (4, 'Data Base', '2020-05-02', '10:00', '11:40', 4, 2);
+INSERT INTO public.schedule (id, course_name, date, start_time, end_time, id_User, id_classroom) VALUES (5, 'Networking', '2020-05-02', '14:00', '15:40', 4, 3);

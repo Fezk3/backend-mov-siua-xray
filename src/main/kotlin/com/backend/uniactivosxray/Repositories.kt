f29afcce9afade83b257f695b1a2c2da1c935577
DiegoEspinoza10 <diego.espinoza.sanchez@est.una.ac.cr>
2023-05-03 10:13:21 -0600

    resolviendo errors por cambios en el entity

--- src/main/kotlin/com/backend/uniactivosxray/Repositories.kt
+++ src/main/kotlin/com/backend/uniactivosxray/Repositories.kt
@@ -20,10 +20,11 @@ interface RoleRepository : JpaRepository<Role, Long> {
 interface AssetsRepository : JpaRepository<Assets, Long> {
     fun findByName(@Param("name") name : String) : Optional<Assets>
 
-    fun findByClassroomlistId(@Param("idClassroom") idClassroom: Long): List<Assets>
+    fun findByClassroom_Id(@Param("classroomId") classroomId: Long): List<Assets>
 
-    @Query("SELECT a FROM Assets a WHERE a.classroomlist IN (SELECT c FROM Classroom c WHERE c.classNumber = :classNumber)")
+    @Query("SELECT a FROM Assets a WHERE a.classroom IN (SELECT c FROM Classroom c WHERE c.classNumber = :classNumber)")
     fun findByClassroomNumber(@Param("classNumber") classNumber: String): List<Assets>
+
 
 }
 

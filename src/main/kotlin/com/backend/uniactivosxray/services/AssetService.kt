0231710b9ea0f3d2e3fd5f5a95af738c48a17c52
Mauren M <mau18.m@gmail.com>
2023-05-09 06:14:40 -0600

    updating AssetService

--- src/main/kotlin/com/backend/uniactivosxray/services/AssetService.kt
+++ src/main/kotlin/com/backend/uniactivosxray/services/AssetService.kt
@@ -3,14 +3,12 @@ package com.backend.uniactivosxray.services
 import com.backend.uniactivosxray.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-import java.util.*
-
 
 interface AssetService {
-    fun findAll(): List<AssetsDetails>?
-    fun findByClassroomId(classroomId: Long): List<AssetsDetails>
-    fun findByClassroomNumber(classroomNumber: String): List<AssetsDetails>
+    fun getAll(): List<AssetsDetails>
+    fun getByClassroomId(classroomId: Long): List<AssetsDetails>
+    fun getByClassroomNumber(classroomNumber: String): List<AssetsDetails>
+    fun getdByAssetTypeAndClassroomId(description: String, classroomId: Long): List<AssetsDetails>
 }
 
 @Service
@@ -25,18 +23,23 @@ class AbstractAssetService(
     val assetTypeRepository: AssetTypeRepository,
     ) : AssetService {
 
-    override fun findAll(): List<AssetsDetails> {
-        val assetsList: List<Assets> = assetsRepository.findAll().toList()
+    override fun getAll(): List<AssetsDetails> {
+        val assetsList: List<Assets> = assetsRepository.findAll()
         return assetsMapper.assetsListToAssetsDetailsList(assetsList)
     }
 
-    override fun findByClassroomId(classroomId: Long): List<AssetsDetails> {
-        val assetsList: List<Assets> = assetsRepository.findByClassroomId(classroomId).toList()
+    override fun getByClassroomId(classroomId: Long): List<AssetsDetails> {
+        val assetsList: List<Assets> = assetsRepository.findByClassroomId(classroomId)
         return assetsMapper.assetsListToAssetsDetailsList(assetsList)
     }
 
-    override fun findByClassroomNumber(number: String): List<AssetsDetails> {
-        val assetsList: List<Assets> = assetsRepository.findByClassroomNumber(number).toList()
+    override fun getByClassroomNumber(number: String): List<AssetsDetails> {
+        val assetsList: List<Assets> = assetsRepository.findByClassroomNumber(number)
+        return assetsMapper.assetsListToAssetsDetailsList(assetsList)
+    }
+
+    override fun getdByAssetTypeAndClassroomId(description: String, classroomId: Long): List<AssetsDetails> {
+        val assetsList: List<Assets> = assetsRepository.findByAssetTypeAndClassroomId(description, classroomId)
         return assetsMapper.assetsListToAssetsDetailsList(assetsList)
     }
 

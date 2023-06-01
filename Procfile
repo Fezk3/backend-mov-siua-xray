2d703b5b2068e1acf7fd64e7dd32b24dc676109d
Victor Segura <seguravictor57@gmail.com>
2023-06-01 10:12:00 -0600

    before .jar config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar build/libs/uniactivosxray-1.0.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	application
 	id("org.springframework.boot") version "2.7.10"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
